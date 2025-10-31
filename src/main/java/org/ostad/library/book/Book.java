02c6edc5f798745ee0f4a777a35abe5959ae038c
Rabbi <fazle.rabbi@bracits.com>
2025-10-31 20:42:39 +0600

    use lombok for the entitiy table

--- src/main/java/org/ostad/library/book/Book.java
+++ src/main/java/org/ostad/library/book/Book.java
@@ -6,9 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
     @Id
@@ -31,73 +37,4 @@ public class Book {
 
     @Column(nullable = false)
     private Integer availableCopies;
-
-    protected Book() {
-        // JPA constructor
-    }
-
-    public Book(String title, String author, String publication, String genre, Integer publicationYear, Integer availableCopies) {
-        this.title = title;
-        this.author = author;
-        this.publication = publication;
-        this.genre = genre;
-        this.publicationYear = publicationYear;
-        this.availableCopies = availableCopies;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getPublication() {
-        return publication;
-    }
-
-    public void setPublication(String publication) {
-        this.publication = publication;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public Integer getPublicationYear() {
-        return publicationYear;
-    }
-
-    public void setPublicationYear(Integer publicationYear) {
-        this.publicationYear = publicationYear;
-    }
-
-    public Integer getAvailableCopies() {
-        return availableCopies;
-    }
-
-    public void setAvailableCopies(Integer availableCopies) {
-        this.availableCopies = availableCopies;
-    }
 }
